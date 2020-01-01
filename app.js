25ee74790d5eec38d060a2cceb8a62b2f56957c4
usr11011 <usr11011@freehost0827.local>
2020-01-02 01:27:27 +0700

    update comments

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Add declared environment values */
-    const env = require('dotenv').config();
+    const env = require('dotenv').config(); // load .env file using dotenv node js module
     //console.log('env'+env);
     //console.log(env.parsed.WITAI_APP_API_TOKEN);
     if (env.error)
@@ -11,13 +11,13 @@
     const SlackBot = require('slackbots');
 
 /* Create clients */
-    const CWitai = new Wit({
-        accessToken: process.env.WITAI_APP_API_TOKEN
+    const CWitai = new Wit({ //
+        accessToken: process.env.WITAI_APP_API_TOKEN // access WITAI_APP_API_TOKEN from .env file
     });
     //console.log(CWitai.message("How are you?"));
     const CSlackBot = new SlackBot({
         token: process.env.SLACK_APP_API_TOKEN,
-        name: 'GoodBot'
+        name: 'GoodBot' // name of bot that will response to your expression in slack app
     })
     
     /*CSlackBot.on('start', function() {
@@ -27,18 +27,18 @@
         CSlackBot.postMessageToChannel('web-development', 'Hello, Everyone!', params);
     });*/
 
-    var numPost = 0;
-    CSlackBot.on('message', function(data) {
+    var numPost = 0; // should not response to the message event
+    CSlackBot.on('message', function(data) { // keep track when any message event happen in the channel
         if (data.type == 'hello') {
             const params = {
                 icon_emoji: ':squirrel:'
             };
-            CSlackBot.postMessageToChannel('web-development', 'Hello!', params);
+            CSlackBot.postMessageToChannel('web-development', 'Hello!', params); // use method provided by slackbots node js module
         }
         if (data.type != 'message') {
             return;
         } else {
-            if (numPost == 1) {
+            if (numPost == 1) { // should response to the message event
                 responseBack(data.text);
             }
             numPost = 1;
@@ -46,7 +46,7 @@
     });
 
     function responseBack(msg) {
-        CWitai.message(msg, {}).then((data) => {
+        CWitai.message(msg, {}).then((data) => { // use method provided by node-wit node js module
             var ent = Object.keys(data.entities);
             console.log('All entity it is in : '+ent);
             // choose the last entity as its type
@@ -60,7 +60,7 @@
             var response = "Sorry! I do not know how to response to \""+msg+"\"";
             switch (lent) {
                 case 'datetime':
-                    var d = data.entities.datetime[0];
+                    var d = data.entities.datetime[0]; // extract datetime
                     var date = new Date(d.value).toDateString();
                     response = date;
                     break;
@@ -89,7 +89,7 @@
 
     var greetings = ["Hey", "Hi", "Hey man!", "Hello!", "Hello, there!", "Yo!", "Good Day!", "Howdy!", "Hey, there!"];
     var thanks = ["You're welcome.", "No problem", "No worries", "Don't mention it", "My pleasure", "Anytime", "Ok", "That's fine"];
-    function getRandomExpr(expr) {
+    function getRandomExpr(expr) { // generate random answer from the array and return it to user
         var what = greetings;
         if (expr == 'thanks') {
             what = thanks;
